5d338714cca6efd10f973bfaf690893e739f5a7f
kosyakovds <kosyakov27@mail.ru>
2025-11-13 20:42:41 +0500

    Task5 V21

---
+++ Tyuiu.KosyakovDS.Sprint4.Task5.V21.Test/DataServiceTest.cs
@@ -0,0 +1,29 @@
+﻿using Tyuiu.KosyakovDS.Sprint4.Task5.V21.Lib;
+
+namespace Tyuiu.KosyakovDS.Sprint4.Task5.V21.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckValid()
+        {
+            DataService ds = new DataService();
+
+            int[,] matrix = new int[5, 5] { { 2, 2, 2, 1, 1 },
+                                            { 2, 2, 2, 1, 1 },
+                                            { 2, 2, 2, 2, 1 },
+                                            { 2, 1, 2, 2, 1 },
+                                            { 2, 1, 2, 2, 1 } };
+            ds.Calculate(matrix);
+            int[,] wait = new int[5, 5] { { 1, 1, 1, 1, 1 },
+                                          { 1, 1, 1, 1, 1 },
+                                          { 1, 1, 1, 1, 1 },
+                                          { 1, 1, 1, 1, 1 },
+                                          { 1, 1, 1, 1, 1 } };
+
+            CollectionAssert.AreEqual(matrix, wait);
+
+        }
+    }
+}
---
+++ Tyuiu.KosyakovDS.Sprint4.Task5.V21/Program.cs
@@ -0,0 +1,58 @@
+﻿using System.Data.Common;
+using Tyuiu.KosyakovDS.Sprint4.Task5.V21.Lib;
+
+namespace Tyuiu.KosyakovDS.Sprint4.Task5.V21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            DataService ds = new DataService();
+            Console.WriteLine("**********************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                               *");
+            Console.WriteLine("**********************************************************************************");
+
+            int[,] matrix = new int[5, 5];
+
+            int rows = matrix.GetLength(0);
+            int columns = matrix.GetLength(1);
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    matrix[i, j] = rnd.Next(-8, 3);
+                }
+            }
+
+            Console.WriteLine("Исходный массив:");
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write($"{matrix[i, j]} \t");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("**********************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                     *");
+            Console.WriteLine("**********************************************************************************");
+
+            ds.Calculate(matrix);
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write($"{matrix[i, j]} \t");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+            Console.ReadKey();
+
+        }
+    }
+}
