d873eea1f929096ae75379cebd33a385813e3af5
kosyakovds <kosyakov27@mail.ru>
2025-11-13 20:10:34 +0500

    Task3 V17

---
+++ Tyuiu.KosyakovDS.Sprint4.Task3.V17.Lib/DataService.cs
@@ -0,0 +1,22 @@
+﻿using System.Numerics;
+using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.KosyakovDS.Sprint4.Task3.V17.Lib
+{
+    public class DataService : ISprint4Task3V17
+    {
+        public int Calculate(int[,] array)
+        {
+            int rows = array.GetLength(0);
+            int columns = array.GetLength(1);
+
+            int sum = 0;
+
+            for (int i = 0; i < rows; i++)
+            {
+                sum += array[i, 3];
+            }
+            return sum;
+        }
+    }
+}
---
+++ Tyuiu.KosyakovDS.Sprint4.Task3.V17/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Data.Common;
+using Tyuiu.KosyakovDS.Sprint4.Task3.V17.Lib;
+
+namespace Tyuiu.KosyakovDS.Sprint4.Task3.V17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.WriteLine("**********************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                               *");
+            Console.WriteLine("**********************************************************************************");
+
+            int[,] matrix = new int[5, 5] { { 6, 4, 2, 2, 1 },
+                                            { 3, 6, 5, 4, 1 },
+                                            { 5, 2, 3, 1, 6 },
+                                            { 8, 8, 4, 5, 3 },
+                                            { 7, 4, 5, 1, 6 } };
+
+            int rows = matrix.GetLength(0);
+            int columns = matrix.GetLength(1);
+
+            Console.WriteLine("Исходный массив:");
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write($"{matrix[i, j]} \t");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("**********************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                     *");
+            Console.WriteLine("**********************************************************************************");
+
+            int res = ds.Calculate(matrix);
+            Console.WriteLine(res);
+
+            Console.WriteLine();
+            Console.ReadKey();
+
+        }
+    }
+}
